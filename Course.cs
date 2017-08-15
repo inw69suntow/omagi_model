9caa0491e25c381a1f8b6f62d00c78fc9abe89d6
yutthana <yutthana.saneha@ktb.co.th>
2017-08-15 17:14:50 +0700

    object สำหรับ ห่อ ข้อมูล

---
+++ Course.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace omagi_model
+{
+    public class Course
+    {
+        public String Topic { get; set; }
+    }
+}
