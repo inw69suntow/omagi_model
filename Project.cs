42285d4b65d425962b9c7e7a65401ece54dd271c
yutthana <yutthana.saneha@ktb.co.th>
2017-08-28 18:00:18 +0700

    xxxxxxxx

---
+++ Project.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace omagi_model
+{
+    public class Project
+    {
+        private String Fl_id {get;set;}
+        private String GroupName { get; set; }
+        private String ParentName { get; set; }
+        private String MainGroupId { get; set; }
+        private String MainGroupName { get; set; }
+        private String ProvinceName { get; set; }
+        private String DeptName { get; set; }
+        private String ReportMember { get; set; }
+        private String Fl_dept { get; set; }
+        private String ParentId { get; set; }
+        private int Level { get; set; }
+
+
+    }
+}
